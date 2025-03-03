7706ba98a1e5cb533fb01b2eb99b28a56637ad29
yeliu-sys <yl13031@nyu.edu>
2025-03-03 18:47:57 -0500

    Update data_processing.py

--- data processing/data_processing.py
+++ data processing/data_processing.py
@@ -26,8 +26,7 @@ import seaborn as sns
 sns.set_theme(style="whitegrid")
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 
-plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
+plt.rcParams['axes.unicode_minus'] = False 
 
 # change text color
 import colorama
@@ -35,25 +34,6 @@ from colorama import Fore, Style
 
 # IPython
 from IPython.display import IFrame
-
-def ts_process_CHINA(ts_data,clip = False):
-    ts_data = ts_data[ts_data['Country/Region'] == 'China']
-    # get loc
-    loc_data = ts_data[['Province/State','Lat','Long']]
-    loc_data = loc_data[loc_data['Province/State'] != 'Unknown']
-
-    ts_data = ts_data.set_index('Province/State').drop(['Country/Region','Lat','Long'],axis=1)
-    if clip:
-        ts_data = ts_data.diff(axis=1).fillna({ts_data.columns[0]:ts_data[ts_data.columns[0]]},downcast = 'infer').clip(lower=0)
-    else:
-        ts_data = ts_data.diff(axis=1).fillna({ts_data.columns[0]:ts_data[ts_data.columns[0]]},downcast = 'infer')
-    ts_data = ts_data.sort_values(by = ts_data.columns[-1],ascending=False)
-    ts_data = ts_data.T.dropna().drop('Unknown',axis=1)
-    ts_data.index = pd.to_datetime(ts_data.index)
-    ts_data.columns.name = ''
-    sorted_provinces = ts_data.columns
-    
-    return ts_data,loc_data,sorted_provinces 
 
 def ts_process_US(ts_data, death = False, clip = False):
     # get loc
@@ -83,20 +63,6 @@ def ts_process_US(ts_data, death = False, clip = False):
     
     return ts_data,loc_data,sorted_state
 
-def daily_process(daily_data, country = 'China'):
-    if country == 'China':
-        daily_data = daily_data[(daily_data['Country_Region'] == 'China')&(daily_data['Province_State']!='Unknown')]
-    elif country == 'US':
-        daily_data = daily_data
-    cols_use = ['Province_State','Last_Update','Confirmed','Deaths','Incident_Rate','Case_Fatality_Ratio']
-
-    
-    daily_data_processed = daily_data[cols_use].set_index(daily_data['Province_State'])
-    daily_data_processed.index.name = ""
-    daily_data_processed = daily_data_processed.dropna()
-    
-    
-    return daily_data_processed
 
 
 
